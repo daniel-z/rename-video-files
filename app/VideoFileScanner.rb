d5d627eb9103e7fca8bccc901595a2429ecddf73
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2012-09-22 03:23:53 -0500

    getVideoSubDirectories - comming back to directory

--- app/VideoFileScanner.rb
+++ app/VideoFileScanner.rb
@@ -4,12 +4,16 @@ class VideoFileScanner
 
   def getVideoSubDirectories ( videoParentFolder )
     allDirectories = [];
+    firstDirectory = Dir.pwd;
+
     Dir.chdir( videoParentFolder );
     Dir.glob("[^a-z]").each do |entry|
       if ( File.directory?( entry ) )
         allDirectories.push( entry );
       end
     end
+
+    Dir.chdir( firstDirectory );
     return allDirectories;
   end
 
