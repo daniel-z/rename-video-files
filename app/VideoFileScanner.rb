df9fca3071c696db1087a8ddced3b35669df4d5e
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2012-09-22 03:20:22 -0500

    Adding getVideoSubDirectories code and test.

--- app/VideoFileScanner.rb
+++ app/VideoFileScanner.rb
@@ -1,12 +1,23 @@
 #
 
 class VideoFileScanner
-  def getVideoSubFolders ( parentVideoFolder )
-    puts 'a'
+
+  def getVideoSubDirectories ( videoParentFolder )
+    allDirectories = [];
+    Dir.chdir( videoParentFolder );
+    Dir.glob("[^a-z]").each do |entry|
+      if ( File.directory?( entry ) )
+        allDirectories.push( entry );
+      end
+    end
+    return allDirectories;
   end
-  def getVideoFilesInFolder ( folder )
+
+  def getVideosInDirectory ( folder )
     
   end
+
   def mapAllFilesParentInFolder ()
   end
+
 end
--- tests/VideoFileScannerTest.rb
+++ tests/VideoFileScannerTest.rb
@@ -2,25 +2,36 @@ require "test/unit"
 require "app/VideoFileScanner"
 
 class VideoFileScannerTest < Test::Unit::TestCase
-  def initialize
-    @videoParentFolder = "testSamples/sample1";
-    @videoSubFolder1   = "testSamples/sample1/1";
-    @videoSubFolder2   = "testSamples/sample1/2";
-    @videoFileScannerObj = VideoFileScanner.new
-    super
-  end
-  
+
   def setup
+    @rootDirectory        = Dir.pwd;
+    @testDirectory        = @rootDirectory        + "/tests/";
+    @testSamplesDirectory = @testDirectory        + "testSamples/";
+    @videoParentFolder    = @testSamplesDirectory + "sample1/";
+    @videoSubFolder1      = @videoParentFolder    + "1";
+    @videoSubFolder2      = @videoParentFolder    + "2";
+    @videoFileScannerObj  = VideoFileScanner.new;
+    puts @rootDirectory;
     super
   end
-  
+
   def teardown
+    Dir.chdir( @rootDirectory )
     super
   end
-  
-  def test_getFolders
+
+  def test_getVideoSubDirectories
       expected = [ "1" , "2" ];
-      assert_equal @videoFileScannerObj.getVideoSubFolders @videoParentFolder, expected
+      assert_equal expected , @videoFileScannerObj.getVideoSubDirectories(@videoParentFolder)
   end
+#  def test_getVideosInDirectory
+#      expected = [
+#        "MOD001.MOD" , "MOD002.MOD" , "MOD003.MOD" , "MOD004.MOD" , "MOD005.MOD" ,
+#        "MOD006.MOD" , "MOD007.MOD" , "MOD008.MOD" , "MOD009.MOD" , "MOD00A.MOD" , 
+#        "MOD00B.MOD" , "MOD00C.MOD" , "MOD00D.MOD" , "MOD00E.MOD" , "MOD00F.MOD" , 
+#        "MOD010.MOD" , "MOD011.MOD" , "MOD012.MOD"
+#      ];
+#      assert_equal expected , @videoFileScannerObj.getVideosInDirectory(@videoParentFolder)
+#  end
 
 end
